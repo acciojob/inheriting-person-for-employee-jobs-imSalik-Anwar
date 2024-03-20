3280fc03204b5228463ac292ac9087a589dcdd62
Salik Anwar <135861792+imSalik-Anwar@users.noreply.github.com>
2024-03-20 15:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+    this.greet = function(){
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+    }
+}
+// Person.prototype.greet = function(){
+// 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+// }
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+	this.jobGreet = function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+}
+Object.setPrototypeOf(Employee, Person);
 
 // Do not change code below this line
 window.Person = Person;
